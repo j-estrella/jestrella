3ee78fa6e32848b127403c0fab012293236cfbc8
Zach Estrella <zachgabriel.estrella@ziaplex.biz>
2025-02-22 03:31:39 +0800

    Add work with me in footer for mobile view

--- src/assets/components/Footer.js
+++ src/assets/components/Footer.js
@@ -11,12 +11,12 @@ export default function Footer(){
 
     return(
         <>
-            <div className='bg-black md:mt-40 mt-20 relative bottom-0 py-16 md:h-[900px] h-auto flex justify-center items-center' id="footer"> {/*  FOOTER */}
+            <div className='bg-black md:mt-40 mt-20 relative bottom-0 py-16 md:h-[800px]' id="footer"> {/*  FOOTER */}
                 <section className="grid md:grid-cols-3 grid-cols-2 grid-rows-2 gap-12 text-white h-[550px]">
 
                     <div className="col-span-1 col-start-1 row-span-2 row-start-1 relative">
                     <img src={dp2} alt="Seb pic 2" className="w-full md:h-3/5 h-2/5 md:object-none object-contain" />
-                    <p className="text-4xl md:text-6xl font-bold leading-tight absolute bottom-0 md:block hidden">WORK WITH ME</p>
+                    <p className="text-4xl md:text-6xl font-bold leading-tight absolute md:bottom-0 bottom-32">WORK WITH ME</p>
                     </div>
 
 
@@ -25,33 +25,31 @@ export default function Footer(){
                     </div>
 
                     <div className="col-span-1 md:col-start-3 col-start-2 row-span-2 flex flex-col h-full">
-                    <div className="self-start w-full">
-                        <hr className="w-full border mb-4" />
-                    </div>
-
-                    <div className="flex flex-col md:justify-center flex-grow gap-6 md:mt-0 mt-8">
-                        <div className="flex gap-4">
-                            <MdOutlineEmail className="text-3xl lg:text-5xl" />
-                            <p className="flex-1 break-all">
-                                Email <br />
-                                <span className="uppercase text-sm tracking-wide">
-                                    <a href="mailto:josh.sebastiene@yahoo.com">josh.sebastiene@yahoo.com</a>
-                                </span>
-                            </p>
+                        <div className="self-start w-full">
+                            <hr className="w-full border mb-4" />
                         </div>
 
-                        <div className="flex gap-4">
-                            <CiPhone className="text-3xl lg:text-5xl" />
-                            <p className="flex-1 break-all">
-                                Phone <br />
-                                <span className="uppercase text-sm tracking-wide">
-                                    <a href="https://wa.me/971554078558?text=Hello%20there!" target="_blank">+971 55-407-8558</a>
-                                </span>  
-                            </p>
-                        </div>
-                    </div>
-                        
+                        <div className="flex flex-col md:justify-center flex-grow gap-6 md:mt-0 mt-8">
+                            <div className="flex gap-4">
+                                <MdOutlineEmail className="text-3xl lg:text-5xl" />
+                                <p className="flex-1 break-all">
+                                    Email <br />
+                                    <span className="uppercase text-sm tracking-wide">
+                                        <a href="mailto:josh.sebastiene@yahoo.com">josh.sebastiene@yahoo.com</a>
+                                    </span>
+                                </p>
+                            </div>
 
+                            <div className="flex gap-4">
+                                <CiPhone className="text-3xl lg:text-5xl" />
+                                <p className="flex-1 break-all">
+                                    Phone <br />
+                                    <span className="uppercase text-sm tracking-wide">
+                                        <a href="https://wa.me/971554078558?text=Hello%20there!" target="_blank">+971 55-407-8558</a>
+                                    </span>  
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </section>
 
